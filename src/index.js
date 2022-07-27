2002bdaa4f32bf0f704f029ff157e43bfbf3dd72
Kisliankou Viachaslau <kislenkov.vs@gmail.com>
2022-07-27 18:19:13 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('');
+  arr.reverse();
+  if (arr[arr.length-1] === '-' || arr[arr.length-1] === '+' ) {
+    arr.pop();  
+  }
+  return +(arr.join(''));
 }
